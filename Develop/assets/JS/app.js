05fe04405c76c7946433874163fd16e79dcb014d
kcenters <kristincenters@gmail.com>
2020-03-08 16:43:29 -0500

    added table with input and save button

--- Develop/assets/JS/app.js
+++ Develop/assets/JS/app.js
@@ -1,4 +1,11 @@
-var now = moment().format('LLL');
+$(document).ready(function() {
+	var now = moment().format('LLL');
 
-var currentDay = document.getElementById('currentDay');
-currentDay.innerHTML = now;
+	var currentDay = document.getElementById('currentDay');
+	currentDay.innerHTML = now;
+});
+
+// Get value on button click and show alert
+$('#saveBtn').click(function() {
+	var userSave = $('#userNotes').val();
+});
